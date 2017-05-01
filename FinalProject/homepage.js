b01612da8fc0490ddbb0df0ce09cf1c484f7e30b
ahmadatieh <aatieh123@gmail.com>
2017-04-30 20:15:17 -0400

    Initial commit
    Full project with missing Play feature

---
+++ FinalProject/homepage.js
@@ -0,0 +1,6 @@
+function clicked(something){
+    var ele = document.getElementById(something);   
+     window.scrollTo(ele.offsetLeft,ele.offsetTop);
+}
+
+//document.getElementById('id').scrollIntoView();
---
+++ FinalProject/js/index.js
@@ -0,0 +1,157 @@
+// Memory Game
+// © 2014 Nate Wiley
+// License -- MIT
+// best in full screen, works on phones/tablets (min height for game is 500px..) enjoy ;)
+// Follow me on Codepen
+
+(function(){
+	
+	var Memory = {
+
+		init: function(cards){
+			this.$game = $(".game");
+			this.$modal = $(".modal");
+			this.$overlay = $(".modal-overlay");
+			this.$restartButton = $("button.restart");
+			this.cardsArray = $.merge(cards, cards);
+			this.shuffleCards(this.cardsArray);
+			this.setup();
+		},
+
+		shuffleCards: function(cardsArray){
+			this.$cards = $(this.shuffle(this.cardsArray));
+		},
+
+		setup: function(){
+			this.html = this.buildHTML();
+			this.$game.html(this.html);
+			this.$memoryCards = $(".card");
+			this.binding();
+			this.paused = false;
+     	this.guess = null;
+		},
+
+		binding: function(){
+			this.$memoryCards.on("click", this.cardClicked);
+			this.$restartButton.on("click", $.proxy(this.reset, this));
+		},
+		// kinda messy but hey
+		cardClicked: function(){
+			var _ = Memory;
+			var $card = $(this);
+			if(!_.paused && !$card.find(".inside").hasClass("matched") && !$card.find(".inside").hasClass("picked")){
+				$card.find(".inside").addClass("picked");
+				if(!_.guess){
+					_.guess = $(this).attr("data-id");
+				} else if(_.guess == $(this).attr("data-id") && !$(this).hasClass("picked")){
+					$(".picked").addClass("matched");
+					_.guess = null;
+				} else {
+					_.guess = null;
+					_.paused = true;
+					setTimeout(function(){
+						$(".picked").removeClass("picked");
+						Memory.paused = false;
+					}, 600);
+				}
+				if($(".matched").length == $(".card").length){
+					_.win();
+				}
+			}
+		},
+
+		win: function(){
+			this.paused = true;
+			setTimeout(function(){
+				Memory.showModal();
+				Memory.$game.fadeOut();
+			}, 1000);
+		},
+
+		showModal: function(){
+			this.$overlay.show();
+			this.$modal.fadeIn("slow");
+		},
+
+		hideModal: function(){
+			this.$overlay.hide();
+			this.$modal.hide();
+		},
+
+		reset: function(){
+			this.hideModal();
+			this.shuffleCards(this.cardsArray);
+			this.setup();
+			this.$game.show("slow");
+		},
+
+		// Fisher--Yates Algorithm -- http://bost.ocks.org/mike/shuffle/
+		shuffle: function(array){
+			var counter = array.length, temp, index;
+	   	// While there are elements in the array
+	   	while (counter > 0) {
+        	// Pick a random index
+        	index = Math.floor(Math.random() * counter);
+        	// Decrease counter by 1
+        	counter--;
+        	// And swap the last element with it
+        	temp = array[counter];
+        	array[counter] = array[index];
+        	array[index] = temp;
+	    	}
+	    	return array;
+		},
+
+		buildHTML: function(){
+			var frag = '';
+			this.$cards.each(function(k, v){
+				frag += '<div class="card" data-id="'+ v.id +'"><div class="inside">\
+				<div class="front"><img src="'+ v.img +'"\
+				alt="'+ v.name +'" /></div>\
+				<div class="back"><img src="http://news.unchealthcare.org/empnews/att/2012/nov/well/@@images/1d83b48a-1952-4723-a23b-b3a9b5e4aa93.png"\
+				alt="Codepen" /></div></div>\
+				</div>';
+			});
+			return frag;
+		}
+	};
+
+	var cards = [
+		{
+			name: "seal",
+			img: "http://www.unc.edu/files/2013/05/UNC.seal_.280-230.jpg",
+			id: 1,
+		},
+		{
+			name: "logo",
+			img: "http://www.unc.edu/files/2012/06/ccm1_032685.jpg",
+			id: 2
+		},
+		{
+			name: "belltower",
+			img: "http://pictures.replayphotos.com/images/UNC/smd/university-of-north-carolina-campus-automatically-imported-morehead-patterson-bell-tower-unc-cp-auto-00018smd.jpg",
+			id: 3
+		},
+		{
+			name: "tarheel",
+			img: "https://s-media-cache-ak0.pinimg.com/originals/a0/f9/4d/a0f94ddca254d5658ae611211e6246fc.jpg",
+			id: 4
+		}, 
+		{
+			name: "rameses",
+			img: "http://blogs.lib.unc.edu/hill/wp-content/uploads/2016/03/Current-Rameses-.jpg",
+			id: 5
+		},
+		{
+			name: "basketball",
+			img: "https://upload.wikimedia.org/wikipedia/commons/thumb/7/7a/Basketball.png/170px-Basketball.png",
+			id: 6
+		},
+		
+		
+	];
+    
+	Memory.init(cards);
+
+
+})();
