a6ac081b1dfde487cf3c4f2c60058060f6f11ff3
ahmadatieh <aatieh123@gmail.com>
2017-05-04 12:51:09 -0400

    Update xlabs.js

--- FinalProject/xlabs.js
+++ FinalProject/xlabs.js
@@ -112,6 +112,29 @@ window.xLabs = {
     console.log( "xLabs: Clearing calibration..." );
   },
 
+  
+  // Student added functionality///
+  
+  
+  var Demo = {
+
+   update : function() {
+     var x = xLabs.getConfig( "state.head.x" );
+     var y = xLabs.getConfig( "state.head.y" );
+     var targetElement = document.getElementById( "target" );
+     targetElement.style.left = ( screen.width * 0.5 ) - ( x * 300 );
+     targetElement.style.top = ( screen.height * 0.5 ) + ( y * 300 );
+   },
+
+   ready : function() {
+     xLabs.setConfig( "system.mode", "head" );
+     xLabs.setConfig( "browser.canvas.paintHeadPose", "0" );
+   }
+
+ };
+  
+  
+  
   ///////////////////////////////////////////////////////////////////////////////////////////////////
   // Time - in a compatible format.
   ///////////////////////////////////////////////////////////////////////////////////////////////////
@@ -328,6 +351,10 @@ window.xLabs = {
   }
 
 };
+
+
+
+
 
 // Usage: xLabs.setup( myCallbackFnReady, myCallbackFnState );
 
